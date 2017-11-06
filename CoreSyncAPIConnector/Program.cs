580f7ac523835cbe3d8427b5a1328b6808ca7172
Unknown <yries.warrior@gmail.com>
2017-11-06 21:20:55 +0100

    Add template

---
+++ CoreSyncAPIConnector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreSyncAPIConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
